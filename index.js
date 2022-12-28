75a3fcb6a56f52d8cdec8010d167b68e3c7007a1
RedEye75 <yusufbinemamstk@gmail.com>
2022-12-28 21:47:56 +0600

    basic server and database setup complete

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 
+// dot env require you must do that
+require("dotenv").config();
+
 // middlewires
 app.use(cors());
 app.use(express.json());
+
+// console.log(process.env.DB_USER);
+// console.log(process.env.DB_PASSWORD);
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.petbnp7.mongodb.net/?retryWrites=true&w=majority`;
+// console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 
 app.get("/", (req, res) => {
   res.send("AceToDo server is now running");
